24a32c30ed6328a46fb0f897258db6c12dcbbc6d
zhanaras <zhnrasbyva3@gmail.com>
2019-02-08 00:47:19 +0600

    added lab2

---
+++ week2/polindrome/polindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            char[] arr = word.ToCharArray();
+            string reverse = String.Empty;
+            //reversing the string
+            for (int i=arr.Length -1; i>-1; i--)
+            {
+                reverse += arr[i];
+            }
+            if (reverse == word)
+            {
+                Console.WriteLine("Yes");
+            }
+            else Console.WriteLine("No");
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ week2/task2/task2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String line;
+            
+            //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("C:\\input.txt");
+            
+
+                //Read the first line of text
+                line = sr.ReadToEnd();
+
+                int[] array = new int[line.Length];
+                string[] s = line.Split();
+
+                //Continue to read until you reach end of file
+                for(int i=0; i<500500; i++)
+                {
+                    array[i] = int.Parse(s[i]);
+                }
+               
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter (@"C:\\output.txt"))
+
+            for (int i=3; i<=(int)Math.Floor(Math.Sqrt(array[i])); i += 2)
+                {
+                   if(array[i] % i == 0)
+                {
+                        file.WriteLine(array[i]);
+                }
+                }
+
+
+            Console.ReadKey();
+        }
+    }
+}
