472167fce2837ebdd018df56176528df6ac4328f
zhanaras <zhnrasbyva3@gmail.com>
2019-02-14 13:31:44 +0600

    added task4

---
+++ week2/task4/task 4/task 4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //writing somethimg to our text file
+            StreamWriter sw = new StreamWriter(@"C:\Users\user\pp22\week2\task4\path\ex.txt");
+            sw.Write("hi");
+            sw.Close();
+            string path = (@"C:\Users\user\pp22\week2\task4\path\ex.txt");
+            string path1 = (@"C:\Users\user\pp22\week2\task4\path1\ex.txt");
+            //copying the file
+            File.Copy(path, path1);
+            File.Delete(path);
+            sw.Close();
+            Console.ReadKey();
+        }
+    }
+}
